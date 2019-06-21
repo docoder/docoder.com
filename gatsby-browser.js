025ed7980f2a7f28e53e48675fd21e7b3e12b9e3
docoder <docoder@163.com>
2019-06-21 11:39:52 +0800

    Update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,3 @@
 * @Author: docoder
 * @Email:  docoder@163.com
 */
-import './src/styles/index.scss'
--- src/components/head.js
+++ src/components/head.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
+import '../styles/index.scss'
 
 const Head = ({ title }) => {
     const data = useStaticQuery(graphql`
